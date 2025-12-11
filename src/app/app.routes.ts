a0b5c2cd7f4e17cb58cd6eb45a0adc6c3a64e44f
prisonmike <utamas2003@gmail.com>
2025-12-11 20:02:36 +0100

    added: main routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Home',
+    loadComponent: () => import('./pages/home/home').then(m => m.Home)
+  },
+  {
+    path: 'products',
+    title: 'Products',
+    loadComponent: () => import('./pages/products/products').then(m => m.Products)
+  },
+  {
+    path: 'cart',
+    title: 'Cart',
+    loadComponent: () => import('./pages/cart/cart').then(m => m.Cart)
+  },
+  {
+    path: 'login',
+    title: 'Login',
+    loadComponent: () => import('./pages/login/login').then(m => m.Login)
+  },
+  {
+    path: 'register',
+    title: 'Register',
+    loadComponent: () => import('./pages/register/register').then(m => m.Register)
+  },
+  { 
+    path: '**',
+    title: 'Not Found',
+    loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFound) 
+  }
+
+];
