b1fb14ad2437fe3582f55c04e23a886dd3113d3f
prisonmike <utamas2003@gmail.com>
2025-12-11 19:39:12 +0100

    modified: toolbar, added: page wrapper

--- src/app/layout/toolbar/toolbar.ts
+++ src/app/layout/toolbar/toolbar.ts
@@ -1,14 +1,15 @@
 import { Component, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.scss',
 })
 export class Toolbar {
-  readonly appTitle = input.required<string>();
+  
 }
