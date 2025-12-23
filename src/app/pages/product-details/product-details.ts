9a9406c9fab34ca575be924a51b538b04d93591c
prisonmike <utamas2003@gmail.com>
2025-12-23 15:43:45 +0100

    implemented: product detail page, created mixin for favoruite icon styling

--- src/app/pages/product-details/product-details.ts
+++ src/app/pages/product-details/product-details.ts
@@ -1,29 +1,39 @@
 // Angular
 import { Component, computed, inject, input } from '@angular/core';
+import { CurrencyPipe, UpperCasePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 // Material
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
 
-
-//App
+// App
 import { ProductFacade } from '../../core/services/product.facade';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [
+    CurrencyPipe,  
+    RouterLink,
+    MatButtonModule, 
+    MatIconModule, 
+    MatChipsModule
+  ],
   templateUrl: './product-details.html',
   styleUrl: './product-details.scss',
 })
 export default class ProductDetails {
   protected readonly facade = inject(ProductFacade);
 
-  // withComponentInputBinding assign
   readonly id = input.required<string>();
   
   protected product = computed(() => {
     const products = this.facade.products();
     return products.find(p => p.id === Number(this.id()));
   });
-  
-  
 
+  protected toggleFavorite(id: number) {
+    this.facade.toggleFavorite(id);
+  }
 }
