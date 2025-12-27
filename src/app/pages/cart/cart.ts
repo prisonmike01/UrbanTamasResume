0e999168c70941d55c9f8bf757d771b5cd98f3e6
prisonmike <utamas2003@gmail.com>
2025-12-27 10:34:49 +0100

    implemented cart page

--- src/app/pages/cart/cart.ts
+++ src/app/pages/cart/cart.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+// Angular
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+// Material
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+// App
+import { CartService } from '../../core/services/cart.service';
+import { CartProductCard } from '../../shared/components/cart-product-card/cart-product-card';
+import { CartSummary } from '../../shared/components/cart-summary/cart-summary';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [
+    RouterLink,
+    MatButton,
+    MatIcon,
+    CartProductCard,
+    CartSummary
+  ],
   templateUrl: './cart.html',
   styleUrl: './cart.scss',
 })
 export default class Cart {
-
+  protected readonly cartService = inject(CartService);
 }
