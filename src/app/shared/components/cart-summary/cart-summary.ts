47b50525358cede5126db7f9ee2f0950cf1ca594
prisonmike <utamas2003@gmail.com>
2025-12-27 10:52:15 +0100

    added cart summary component

---
+++ src/app/shared/components/cart-summary/cart-summary.ts
@@ -0,0 +1,23 @@
+// Angular
+import { Component, inject } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+
+// Material
+import { MatCardModule } from "@angular/material/card";
+import { MatDivider } from "@angular/material/divider";
+import { MatButton } from '@angular/material/button';
+
+// App
+import { CartService } from '../../../core/services/cart.service';
+
+
+@Component({
+  selector: 'app-cart-summary',
+  imports: [MatCardModule, MatDivider, CurrencyPipe, MatButton],
+  templateUrl: './cart-summary.html',
+  styleUrl: './cart-summary.scss',
+})
+export class CartSummary {
+  protected readonly cartService = inject(CartService);
+
+}
